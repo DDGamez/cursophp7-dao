dea87544a94a7b75ef1495b083eb9ba65a75b634
David <davidddgamez@gmail.com>
2020-06-30 11:51:10 -0300

    inserir e alterar

--- index.php
+++ index.php
@@ -16,18 +16,34 @@
 	//echo json_encode($search);	
 	
 	//carrega os usuario usando ow login e a senha
-	$usuario = new Usuario();
-	$usuario->login("Jonas", "456");
+	//$usuario = new Usuario();
+	//$usuario->login("Jonas", "456");
+	//echo $usuario;
+
+	//iserção de um novo aluno no banco passando os setters	
+	//$aluno = new Usuario();
+	//$aluno->setDeslogin("Aluno");
+	//$aluno->setDessenha("123b456");
+	//$aluno->insert();
+	//echo $aluno;
 	
-	echo $usuario;
-	
-	
-	
-	
+	//iserção de um novo aluno no banco passando os setters	
+	//$aluno = new Usuario("Raissa", "5897");
+	//$aluno->insert();
+	//echo $aluno;
 	
 	//$sql = new Sql();
 	//$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 	//echo json_encode($usuarios);
+	
+	//Alterando um dado atraves do UPDATE
+	$usuario = new Usuario();
+	$usuario->loadbyid(12);
+	
+	$usuario->update("Manu", "13@@@46");
+	
+	echo $usuario;
+	
 	
 ?>
 	
